19f7274f654c09c03cc615cb3e150ca197898742
hempflower <1993996310@qq.com>
2020-04-03 15:56:23 +0800

    Init

---
+++ src/components/WindowBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-system-bar window dark>
+    <v-icon>mdi-message</v-icon>
+    <span>10 unread messages</span>
+    <v-spacer></v-spacer>
+    <v-icon>mdi-minus</v-icon>
+    <v-icon>mdi-checkbox-blank-outline</v-icon>
+    <v-icon>mdi-close</v-icon>
+  </v-system-bar>
+</template>
+
+<script>
+export default {
+    name: 'WindowBar'
+};
+</script>
+
+<style>
+</style>
