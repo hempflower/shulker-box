93453db76a237905b05969c9baadfd653fc538f0
hempflower <1993996310@qq.com>
2020-04-05 16:00:46 +0800

    Change CI config file

--- scripts/electron-publisher-custom.js
+++ scripts/electron-publisher-custom.js
@@ -1,7 +1,32 @@
 const electronPublish = require('electron-publish');
+const OSS = require('ali-oss');
+
+
+const basedir = "download/latest"
 class Publisher extends electronPublish.Publisher {
   async upload(task) {
-    console.log('electron-publisher-orecraft', task.file);
+    //console.log('electron-publisher-orecraft', task.file);
+    if (
+      !process.env['OSS_REGION'] ||
+      !process.env['OSS_ID'] ||
+      !process.env['OSS_SECRET'] ||
+      !process.env['OSS_BUCKET']
+    ) {
+      console.log('Invaild OSS Config.')
+      return;
+    }
+    let client = new OSS({
+      region: process.env['OSS_REGION'],
+      accessKeyId: process.env['OSS_ID'],
+      accessKeySecret: process.env['OSS_SECRET'],
+      bucket: process.env['OSS_BUCKET']
+    });
+    try {
+      let result = await client.put(basedir + '/' + task.safeArtifactName, task.file);
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 module.exports = Publisher;
