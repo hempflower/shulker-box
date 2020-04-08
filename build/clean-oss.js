5de4ec963d45de61b11f4dafcf0ed6798978cb87
hempflower <1993996310@qq.com>
2020-04-08 12:58:02 +0800

    自动清理旧版本文件

---
+++ build/clean-oss.js
@@ -0,0 +1,31 @@
+//旧版本的堆积会大量占用OSS空间
+const OSS = require('ali-oss');
+const path = require('path')
+
+const basedir = "download/latest"
+
+//console.log('electron-publisher-orecraft', task.file);
+if (
+  !process.env['OSS_REGION'] ||
+  !process.env['OSS_ID'] ||
+  !process.env['OSS_SECRET'] ||
+  !process.env['OSS_BUCKET']
+) {
+  console.error('Invaild OSS Config.')
+  return;
+}
+let client = new OSS({
+  region: process.env['OSS_REGION'],
+  accessKeyId: process.env['OSS_ID'],
+  accessKeySecret: process.env['OSS_SECRET'],
+  bucket: process.env['OSS_BUCKET'],
+  timeout: '600000'
+});
+let todelete = []
+const result = client.list({
+  prefix: basedir,
+})
+result.objects.forEach(obj => {
+  todelete.push(obj.name)
+});
+client.deleteMulti(todelete);
