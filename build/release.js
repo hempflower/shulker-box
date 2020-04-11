f7581bbee1289c5a1289b8451ad61b2780f75493
hempflower <1993996310@qq.com>
2020-04-11 12:45:28 +0800

    重构发布流程

---
+++ build/release.js
@@ -0,0 +1,54 @@
+const OSS = require('ali-oss');
+const path = require('path')
+const request = require('superagent')
+const fs = require('fs')
+
+const basedir = "download/latest"
+
+
+if (
+    !process.env['OSS_REGION'] ||
+    !process.env['OSS_ID'] ||
+    !process.env['OSS_SECRET'] ||
+    !process.env['OSS_BUCKET']
+) {
+    console.log(process.env)
+    console.error('Invaild OSS Config.')
+    //return;
+}
+let client = new OSS({
+    region: process.env['OSS_REGION'],
+    accessKeyId: process.env['OSS_ID'],
+    accessKeySecret: process.env['OSS_SECRET'],
+    bucket: process.env['OSS_BUCKET'],
+    timeout: '600000'
+});
+
+getAssets();
+
+function getAssets() {
+
+    request.get("https://api.github.com/repos/hempflower/shulker-box/releases/latest").set('User-Agent', 'releaseBot/1.0.0').end((err, res) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        if (typeof (res.body.assets) == 'undefined') {
+            console.error('No release found.')
+            return;
+        }
+
+        for (let file of res.body.assets) {
+            downloadFile(file['browser_download_url'], file['name'])
+        }
+    })
+
+}
+
+function downloadFile(url, name) {
+    fs.mkdirSync('assets')
+    let stream = fs.createWriteStream('./assets/' + name)
+    request.get(url).set('User-Agent', 'releaseBot/1.0.0').pipe(stream)
+    client.put(basedir + '/' + name, './assets/' + name)
+    console.log('Downloaded ' + name)
+}
--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
           }
         },
         publish: {
-          "provider": "custom"
+          "provider": "github"
         }
       }
     }
