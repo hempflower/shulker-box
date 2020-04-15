1f9fb79a659323c11f170f6cdbdab1525181f6ee
hempflower <1993996310@qq.com>
2020-04-15 20:44:35 +0800

    游戏管理页面 - 参数设置UI

--- src/pages/LauncherGame.vue
+++ src/pages/LauncherGame.vue
@@ -26,7 +26,7 @@
           <v-list-item
             v-for="(value,i) in v.versions"
             :key="i"
-            @click="common(v.path,value)"
+            @click="common(v,value)"
             :input-value="selected.path == v.path && selected.version == value"
             link
           >
@@ -37,31 +37,70 @@
         </v-list-group>
       </v-list>
     </v-card>
-    <v-card class="main_card" color="#1e1e1ec9">
+    <v-card v-show="selected.pathName!=null" class="main_card" color="#1e1e1ec9">
       <v-toolbar dense>
-        <v-toolbar-title>官方启动器/1.15</v-toolbar-title>
+        <v-toolbar-title>{{selected.pathName}}/{{selected.version}}</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-btn icon>
           <v-icon>mdi-dots-vertical</v-icon>
         </v-btn>
         <template v-slot:extension>
-          <v-tabs v-model="tab" show-arrows="">
+          <v-tabs v-model="tab" show-arrows>
             <v-tabs-slider></v-tabs-slider>
-            <v-tab>基本设置</v-tab>
+            <v-tab>参数设置</v-tab>
             <v-tab>自动安装</v-tab>
             <v-tab>MOD管理</v-tab>
             <v-tab>材质包管理</v-tab>
             <v-tab>光影包管理</v-tab>
             <v-tab>存档管理</v-tab>
+            <v-tab>实用工具</v-tab>
           </v-tabs>
         </template>
       </v-toolbar>
+
+      <v-tabs-items v-model="tab" class="non-background scroll-content">
+        <v-tab-item>
+          <BasicConfig :config="selected.config.basic"></BasicConfig>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card class="non-background" flat>
+            <v-card-text>表单页面2</v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card class="non-background" flat>
+            <v-card-text>表单页面3</v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card class="non-background" flat>
+            <v-card-text>表单页面4</v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card class="non-background" flat>
+            <v-card-text>表单页面5</v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card class="non-background" flat>
+            <v-card-text>表单页面6</v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card class="non-background" flat>
+            <v-card-text>表单页面7</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
     </v-card>
   </div>
 </template>
 
 <script>
+import BasicConfig from "./LauncherGame/BasicConfig";
 export default {
+  components: { BasicConfig },
   data() {
     return {
       games: [
@@ -92,17 +131,23 @@ export default {
       ],
       selected: {
         path: null,
-        version: null
+        pathName: null,
+        version: null,
+        config: {
+          basic: {}
+        }
       },
-      tab: null,
+      tab: null
     };
   },
   methods: {
-    common(path, version) {
-      this.selected.path = path;
+    common(pathobj, version) {
+      this.selected.pathName = pathobj.name;
+      this.selected.path = pathobj.path;
       this.selected.version = version;
     }
-  }
+  },
+  mounted() {}
 };
 </script>
 
@@ -125,5 +170,14 @@ export default {
   right: 8px;
   bottom: 8px;
   left: 276px;
+}
+
+.non-background {
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.scroll-content {
+  height: calc(100% - 96px);
+  overflow-y: scroll;
 }
 </style>
---
+++ src/pages/LauncherGame/BasicConfig.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-card class="non-background" flat>
+    <v-alert dense >
+      因为此版本使用了全局配置，所以在此处所做的更改将会保存到全局配置中。
+    </v-alert>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-checkbox v-model="config.global" label="使用全局配置"></v-checkbox>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-subheader>JVM设置</v-subheader>
+          <v-divider></v-divider>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6">
+          <v-combobox :items="javalist" no-filter label="JAVA路径"></v-combobox>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6" md="3">
+          <v-text-field label="最大游戏内存(MB)"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field label="JVM附加参数"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-subheader>Minecraft设置</v-subheader>
+          <v-divider></v-divider>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6">
+          <v-checkbox label="演示模式(DEMO)"></v-checkbox>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6">
+          <v-select :items="gameDir" label="游戏目录"></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="2">
+          <v-text-field label="窗口宽度"></v-text-field>
+        </v-col>
+        <v-col cols="2">
+          <v-text-field label="窗口高度"></v-text-field>
+        </v-col>
+        <v-col cols="2">
+          <v-checkbox label="全屏"></v-checkbox>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3">
+          <v-text-field label="直连服务器地址"></v-text-field>
+        </v-col>
+        <v-col cols="2">
+          <v-text-field label="端口"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3">
+          <v-text-field label="代理服务器地址"></v-text-field>
+        </v-col>
+        <v-col cols="2">
+          <v-text-field label="端口"></v-text-field>
+        </v-col>
+        <v-col cols="3">
+          <v-text-field label="用户名"></v-text-field>
+        </v-col>
+        <v-col cols="3">
+          <v-text-field label="密码"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field label="额外参数"></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["config"],
+  data() {
+    return {
+      javalist: [],
+      gameDir: [
+        {
+          text: "在.minecraft目录下",
+          value: 0
+        },
+        {
+          text: "在.minecraft/versions/<version>目录下",
+          value: 1
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
